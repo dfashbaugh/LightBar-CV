b5ce100220b9a6e58f4245393cdde00a396c631b
Dylan Fashbaugh <dylan@Dylans-MacBook-Pro.local>
2015-10-20 22:45:28 -0400

    Added changes to CVBars to allow for finding green

--- CVBars.cpp
+++ CVBars.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d.hpp>
 
 using namespace std;
 using namespace cv;
@@ -23,8 +25,52 @@ Mat findGreen (Mat myImage)
     medianBlur(myImage, myImage, 31);
     cvtColor(myImage,myImage,CV_BGR2HSV);
     inRange(myImage,Scalar(36,90,183),Scalar(169,255,255),myImage); 
+    //inRange(myImage,Scalar(36,00,0),Scalar(169,255,255),myImage); 
+    bitwise_not(myImage, myImage);
+    //cvtColor(myImage, myImage, CV_HSV2BGR);
+    //cvtColor(myImage, myImage, CV_BGR2GRAY);
 
-    return myImage;
+
+
+    //findContours(myImage, myImage, CV_RETR_EXTERNAL, CV_RETR_EXTERNAL);
+
+    // Test BLOB Code
+    
+    SimpleBlobDetector::Params params;
+    params.minThreshold = 10;
+    params.maxThreshold = 1000;
+    params.filterByArea = false;
+    params.filterByCircularity = false;
+    params.filterByConvexity = false;
+    params.filterByInertia = false;
+
+    //Mat hsv_channels[3];
+    //split( myImage, hsv_channels );
+
+    // Set up the detector with default parameters.
+    //SimpleBlobDetector detector;
+    cv::Ptr<SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+
+    //SURF surf;
+
+    // Detect blobs.
+    std::vector<KeyPoint> keypoints;
+    detector->detect( myImage/*hsv_channels[2]*/, keypoints);
+
+    printf("Keypoints Found %lu... OpenCV Version %d\n", keypoints.size(), CV_MAJOR_VERSION);
+ 
+    // Draw detected blobs as red circles.
+    // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
+    Mat im_with_keypoints;
+    drawKeypoints( myImage, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    
+    /*
+    for(int i = 0; i<keypoints.size(); i++)
+    {
+        printf("Key Point Size = %0.2f.. Location X = %0.2f.. Location Y = %0.2f\n", keypoints[i].size, keypoints[i].pt.x, keypoints[i].pt.y);
+    }
+    */
+    return im_with_keypoints;
 }
 
 int runVideo()
@@ -54,9 +100,9 @@ int runVideo()
             // Process Image
             //medianBlur(myImage, myImage, 11);
             //threshold(myImage, myImage, 160, 255, THRESH_BINARY);
-            cvtColor(myImage,myImage,CV_BGR2HSV);
-            inRange(myImage,Scalar(130,60,90),Scalar(255,255,255),myImage); 
-            
+            //cvtColor(myImage,myImage,CV_BGR2HSV);
+            //inRange(myImage,Scalar(130,60,90),Scalar(255,255,255),myImage); 
+            myImage = findGreen(myImage);
             // Display Image
             imshow("Video", myImage);
         }
